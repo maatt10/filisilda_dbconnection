0ddd02ad07280e7359d366ce689cd744729b70b6
maatt10 <m.filisilda.557934@umindanao.edu.ph>
2026-02-19 20:54:50 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include "db.php";
+ 
+$clients = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) AS c FROM clients"))['c'];
+$services = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) AS c FROM services"))['c'];
+$bookings = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) AS c FROM bookings"))['c'];
+ 
+$revRow = mysqli_fetch_assoc(mysqli_query($conn, "SELECT IFNULL(SUM(amount_paid),0) AS s FROM payments"));
+$revenue = $revRow['s'];
+?>
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <title>Dashboard</title>
+</head>
+<body>
+<?php include "nav.php"; ?>
+
+<div class="wrapper">
+  <h2>Dashboard</h2>
+
+  <ul class="stats">
+    <li>Total Clients <b><?php echo $clients; ?></b></li>
+    <li>Total Services <b><?php echo $services; ?></b></li>
+    <li>Total Bookings <b><?php echo $bookings; ?></b></li>
+    <li>Total Revenue <b>₱<?php echo number_format($revenue,2); ?></b></li>
+  </ul>
+
+  <p class="links">
+    <a href="/assessment_beginner/pages/clients_add.php">Add Client</a>
+    <a href="/assessment_beginner/pages/bookings_create.php">Create Booking</a>
+  </p>
+</div>
+
+</body>
+</html>
