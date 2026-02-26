e979c05df4091005536fa93e9ad6f52a3767f29c
maatt10 <m.filisilda.557934@umindanao.edu.ph>
2026-02-26 23:02:08 +0800

    New pages, new css

--- pages/clients_edit.php
+++ pages/clients_edit.php
@@ -1,19 +1,19 @@
 <?php
 include "../db.php";
- 
+
 $id = $_GET['id'];
- 
+
 $get = mysqli_query($conn, "SELECT * FROM clients WHERE client_id = $id");
 $client = mysqli_fetch_assoc($get);
- 
+
 $message = "";
- 
+
 if (isset($_POST['update'])) {
   $full_name = $_POST['full_name'];
   $email = $_POST['email'];
   $phone = $_POST['phone'];
   $address = $_POST['address'];
- 
+
   if ($full_name == "" || $email == "") {
     $message = "Name and Email are required!";
   } else {
@@ -28,30 +28,34 @@ if (isset($_POST['update'])) {
 ?>
 <!doctype html>
 <html>
+
 <head>
   <link rel="stylesheet" href="../style.css">
-  <meta charset="utf-8"><title>Edit Client</title>
+  <meta charset="utf-8">
+  <title>Edit Client</title>
 </head>
+
 <body>
-<?php include "../nav.php"; ?>
- 
-<h2>Edit Client</h2>
-<p style="color:red;"><?php echo $message; ?></p>
- 
-<form method="post">
-  <label>Full Name*</label><br>
-  <input type="text" name="full_name" value="<?php echo $client['full_name']; ?>"><br><br>
- 
-  <label>Email*</label><br>
-  <input type="text" name="email" value="<?php echo $client['email']; ?>"><br><br>
- 
-  <label>Phone</label><br>
-  <input type="text" name="phone" value="<?php echo $client['phone']; ?>"><br><br>
- 
-  <label>Address</label><br>
-  <input type="text" name="address" value="<?php echo $client['address']; ?>"><br><br>
- 
-  <button type="submit" name="update">Update</button>
-</form>
+  <?php include "../nav.php"; ?>
+
+  <h2>Edit Client</h2>
+  <p style="color:red;"><?php echo $message; ?></p>
+
+  <form method="post">
+    <label>Full Name*</label><br>
+    <input type="text" name="full_name" value="<?php echo $client['full_name']; ?>"><br><br>
+
+    <label>Email*</label><br>
+    <input type="text" name="email" value="<?php echo $client['email']; ?>"><br><br>
+
+    <label>Phone</label><br>
+    <input type="text" name="phone" value="<?php echo $client['phone']; ?>"><br><br>
+
+    <label>Address</label><br>
+    <input type="text" name="address" value="<?php echo $client['address']; ?>"><br><br>
+
+    <button type="submit" name="update">Update</button>
+  </form>
 </body>
+
 </html>
